e83b6b177afabf295caf560c490b77b274422326
Maham-Liaqat <mahamliaqat1234@gmail.com>
2025-07-02 22:54:41 +0500

    Changed the Logo of TextUtils

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   base: '/TextUtils/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
